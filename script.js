53c424ef0415d8d3a5137f76d8953ebee7695697
Heavenpatel5114 <113304844+Heavenpatel5114@users.noreply.github.com>
2023-04-12 10:30:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-function fibonacci(num) {
-// your code here
+function fib(n) {
+    if (n <= 1)
+        return n;
+    return fib(n-1) + fib(n-2);
 }
+  
+    //function call
+    document.write(fib(n));
 
 module.exports = fibonacci;
